72f50ec2fda4f53758254d338ef3da4caeeafa4f
Mudassirdbs <mudassirdbs@gmail.com>
2026-07-29 14:02:44 +0500

    Configure Nitro preset for Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       server: { entry: "server" },
     }),
     nitro({
-      defaultPreset: "cloudflare-module",
+      defaultPreset: "cloudflare-pages",
     }),
     viteReact(),
     tailwindcss(),
